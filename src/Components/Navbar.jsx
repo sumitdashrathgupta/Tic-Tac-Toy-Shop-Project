3c8643a58a7f43a84b3a95e48d6733e535dd8a27
sumitdashrathgupta <sumitdashrathgupta111@gmai.com>
2025-02-26 11:09:35 +0530

    fixed error

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,84 +1,45 @@
-import React, { useState, useEffect } from "react";
-import Logo from "../assets/System Image/Logo.png";
+import React from "react";
+import Logo from "../assets/System Image/Logo.png"
 import { FaShoppingCart } from "react-icons/fa";
 import { TbBaselineDensitySmall } from "react-icons/tb";
-import "../Style/Navbar.css";
+import "../Style/Navbar.css"
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import Login from "./Login";
 
-const Navbar = () => {
-  const cartval = useSelector((state) => state.cart.cartCount);
-  const navigate = useNavigate();
-  const [isLoginOpen, setIsLoginOpen] = useState(false);
-  const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const loggedInUser = localStorage.getItem("user");
-    if (loggedInUser) {
-      setUser(JSON.parse(loggedInUser));
+const Navbar = (props) => {
+    let cartval = useSelector((state) => state.cart.cartCount);
+    let Navigatelogo = useNavigate();
+    let Navigate = useNavigate();
+    const handleCartClick = () => {
+        Navigate(`cart/${props.id - 1}`);
     }
-  }, []);
 
-  const handleLoginSuccess = (userData) => {
-    setUser(userData);
-    setIsLoginOpen(false);
-    localStorage.setItem("user", JSON.stringify(userData));
-  };
+    const handlelogo = () => {
+        Navigatelogo('/')
+    }
 
-  const handleLogout = () => {
-    setUser(null);
-    localStorage.removeItem("user");
-  };
+    return (
+        <>
+            <header className="nav-header">
+                <div className="navbar">
+                    <div className="logo-part" onClick={handlelogo}>
+                        <img className="logo" src={Logo} alt="logo" />
+                        <h1>Tic Tac Toy</h1>
+                    </div>
+                    <input className="input-search" type="search" placeholder="Search the Toy" />
+                    <div className="aside">
+                        <FaShoppingCart className="card-icon" onClick={handleCartClick} />
+                        <span className="cartspan">{cartval}</span>
+                        <button className="btn" type="button">Login</button>
+                    </div>
+                    <div className="mr-icon">
+                        <TbBaselineDensitySmall />
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
 
-  return (
-    <>
-      <header className="nav-header">
-        <div className="navbar">
-          <div className="logo-part" onClick={() => navigate("/")}>
-            <img className="logo" src={Logo} alt="logo" />
-            <h1>Tic Tac Toy</h1>
-          </div>
-          <input
-            className="input-search"
-            type="search"
-            placeholder="Search the Toy"
-          />
-          <div className="aside">
-            <FaShoppingCart
-              className="card-icon"
-              onClick={() => navigate("/cart")}
-            />
-            <span className="cartspan">{cartval}</span>
-
-            {user ? (
-              <>
-                <span className="hello-text">Hello, {"himanshuv07"}</span>
-                {user.username}
-                <button className="btn logout-btn" onClick={handleLogout}>
-                  Logout
-                </button>
-              </>
-            ) : (
-              <button className="btn" onClick={() => setIsLoginOpen(true)}>
-                Login
-              </button>
-            )}
-          </div>
-          <div className="mr-icon">
-            <TbBaselineDensitySmall />
-          </div>
-        </div>
-      </header>
-
-      {isLoginOpen && (
-        <Login
-          onClose={() => setIsLoginOpen(false)}
-          onLoginSuccess={handleLoginSuccess}
-        />
-      )}
-    </>
-  );
-};
-
-export default Navbar;
+export default Navbar
